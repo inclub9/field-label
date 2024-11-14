150f38eb8e76f36f6f5aecd10b2ed46d1fbc8dd4
inclub9 <inclub9@gmail.com>
2024-11-14 22:15:04 +0700

    Revert "optimize"
    This reverts commit 116dd9a6

--- src/main/java/inclub9/entity/User.java
+++ src/main/java/inclub9/entity/User.java
@@ -1,8 +1,12 @@
 package inclub9.entity;
 
 import inclub9.annotation.FieldLabel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
 public class User {
+    @Id
     private Long id;
 
     @FieldLabel("Full Name")
